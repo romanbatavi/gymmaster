78ddb39272d7ebd67a584ac645e57485494203f6
romanbatavi <romanbatavi98@gmail.com>
2021-02-20 21:06:24 +0700

    fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,32 +4,12 @@
   <div class="footer-top">
     <div class="container">
       <div class="row">
-      
         <div class="col-md-6">
           <ul>
           <li><a href="page.php?type=aboutus">About Us</a></li>
             <li><a href="page.php?type=privacy">Privacy Policy</a></li>
           <li><a href="page.php?type=panduan">Panduan Pembayaran</a></li>
           </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-push-6 text-right">
-          <div class="footer_widget">
-            <p>Linik Sosial Media :</p>
-            <ul>
-              <li><a href="https://www.facebook.com/ZeroTech123/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-              <li><a href="https://twitter.com/kostharapan"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-              <li><a href="https://www.instagram.com/zerotech__/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-6 col-md-pull-6">
-          <p class="copy-right">Copyright &copy; 18111098 - Mohammad Faiz Albar - TIF RP 18 CNS A</p>
         </div>
       </div>
     </div>
